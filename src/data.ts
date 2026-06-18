9433d692ef65c4846583d30ad379262658267ad1
milliondreamsblog <114696765+milliondreamsblog@users.noreply.github.com>
2026-06-18 20:48:40 +0530

    feat(data): add GangaPlace interface

---
+++ src/data.ts
@@ -0,0 +1,10 @@
+export interface GangaPlace {
+  id: number;
+  name: string;
+  state: string;
+  description: string;
+  features: string[];
+  cuisine: string;
+  art: string;
+  heritage: string;
+}
